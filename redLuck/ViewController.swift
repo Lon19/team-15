3828dfb13380f320924b920a46c587e0b7fb063f
qsmy41 <ariszyq123@gmail.com>
2019-11-01 21:23:49 +0000

    Initial Commit

---
+++ redLuck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  redLuck
+//
+//  Created by Aris Zhu Yi Qing on 1/11/19.
+//  Copyright © 2019 Aris Zhu Yi Qing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
